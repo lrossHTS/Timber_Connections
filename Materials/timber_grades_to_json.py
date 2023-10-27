ffb909399e755a6c1b115525083dfdfd47630d4d
Liam Ross <LRoss@hts.uk.com>
2023-10-27 21:14:50 +0100

    timber grades and json maker added

---
+++ Materials/timber_grades_to_json.py
@@ -0,0 +1,25 @@
+import pandas
+import json
+import os
+
+dir = os.getcwd()
+print(dir)
+
+
+# Read excel document
+excel_data_df = pandas.read_excel(dir + '\\Materials\\timber_grades.xlsx', sheet_name = 'Sheet1', header = 0, index_col=0)
+
+# excel_data_df.to_dict()
+
+excel_json = excel_data_df.to_json()
+
+# Print out the result
+print('Excel Sheet to JSON:\n', excel_json)
+
+# Make the string into a list to be able to input in to a JSON-file
+thisisjson_dict = json.loads(excel_json)
+
+# Define file to write to and 'w' for write option -> json.dump() 
+# defining the list to write from and file to write to
+with open('timber_grades.json', 'w') as json_file:
+    json.dump(thisisjson_dict, json_file)
