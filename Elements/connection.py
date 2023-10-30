3b7a3f614fdbb40f57c8ad22f60290c018c219c3
Liam Ross <LRoss@hts.uk.com>
2023-10-30 15:16:57 +0000

    further load decomp

--- Elements/connection.py
+++ Elements/connection.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math as m 
 import base_elements as be
 import base_materials as bm
 
@@ -7,7 +8,14 @@ class connection():
         self.fixing = be.bolt(dia, Grade)
         self.loads = {}
 
+    def define_edges():
+        self.edges = {'A': True, 'B':False, 'C':True, 'D':True}
+
+    def grain_dir():
+        self.grain_dir = 'A' # or parallel to B 
+
     def arrange_bolts(self, n_rows, n_cols, a_1, a_2, a_3): 
+        ''' Set coordinates of bolts'''
         # Create grid arrangement
         self.n_bolts = n_rows * n_cols
 
@@ -17,9 +25,10 @@ class connection():
 
         self.group_width = a_1 * (n_cols - 1)
         self.group_height = a_2 * (n_rows - 1)
-        self.group_centre = (self.group_width/2, -self.group_height/2)
+        self.group_centre = (0,0)
 
         self.group_inertia = 0
+        self.r_max = 0
 
         bolts = {}
 
@@ -40,9 +49,13 @@ class connection():
                 dx = self.group_centre[0]-x
                 dy = self.group_centre[1]-y
                 r = (dx**2 + dy**2)**0.5
+
                 self.group_inertia += r**2
 
-                bolts[str(k)] = {'x':x ,'y': y, 'row_id': row_id, 'r': r, 'load':{}}
+                if r > self.r_max:
+                    self.r_max = r
+
+                bolts[str(k)] = {'x':x ,'y': y, 'row_id': row_id, 'r': r, 'load':{}, 'dx':dx, 'dy':dy}
 
                 x += a_1
                 k += 1
@@ -53,33 +66,53 @@ class connection():
         self.bolts = bolts
 
     def plot_bolts(self):
-        ''' Plots bolt arrangement for review'''
+        ''' Plots bolt arrangement for visual review'''
         x = [self.bolts[x]['x'] for x in self.bolts]
         y = [self.bolts[x]['y'] for x in self.bolts]
 
         plt.plot(x,y,'o')
         plt.show()
 
-    def apply_load(self, load_case, V_ed, duration):
+    def apply_load(self, load_case, V_ed, M_ed, duration):
         ''' Stores loadcase info to connection adn calculates ecc mom'''
         ecc = self.group_width/2 + self.a_3 + 50
         M_ecc = V_ed * ecc/1000 
 
-        self.loads[load_case] = {'V_ed': V_ed, 'M_ed': M_ecc, 'duration': duration}
+        self.loads[load_case] = {'V_ed': V_ed, 'M_ed': M_ecc + M_ed, 'duration': duration}
+
+        print('Loadcase {} - Ved = {} kN, Med = {} kNm, Mecc = {} kNm'.format(load_case, V_ed, M_ed, M_ecc))
 
     def decompose_load(self):
+        ''' Determine each load to each bolt'''
+        n_bolts = self.n_bolts
+        
         for load_case in self.loads:
             V_ed = self.loads[load_case]['V_ed']
             M_ed = self.loads[load_case]['M_ed']
 
-            V_ed_i = V_ed / self.n_bolts
+            V_ed_i = V_ed / n_bolts
 
             for i in self.bolts:
-                F_md = M_ed * self.bolts[i]['r'] / self.group_inertia
+                # Calc forces due to moment
+                F_m = M_ed * 1000 * self.bolts[i]['r'] / self.group_inertia
+                F_mx = F_m * self.bolts[i]['dy'] / self.r_max *-1
+                F_my = F_m * self.bolts[i]['dx'] / self.r_max
 
+                # Determine net components and resultant
+                F_x = F_mx
+                F_y = V_ed_i + F_my
 
-                self.bolts[i]['load'][load_case] = {'F_vd': V_ed_i, 'F_md': F_md}
-       
+                F_res = ((F_x)**2 + (F_y)**2)**0.5
+                if F_x == 0:
+                    alpha = m.pi
+                else:
+                    alpha = m.atan(F_y/F_x)
+                
+                alpha = m.degrees(alpha)
+
+                self.bolts[i]['load'][load_case] = {'F_x': F_x, 'F_y': F_y, 'F_res':F_res, 'alpha': alpha}
+                print('Bolt {} - F_x = {} kN, F_y = {} kN, F_res = {} kN, alpha = {} deg'.format(i, round(F_x), round(F_y), round(F_res), round(alpha)))
+
 if __name__ == "__main__":
 
     fixing_grade = '8.8'
@@ -87,10 +120,10 @@ if __name__ == "__main__":
 
     cxxn = connection(fixing_grade, dia)
 
-    cxxn.arrange_bolts(5 , 5, 100, 100)
+    cxxn.arrange_bolts(2 , 2, 100, 100, 50)
     # cxxn.plot_bolts()
-    cxxn.apply_load('LC 1', 100, 'Permanent')
-    cxxn.apply_load('LC 2', 50, 'instantaneous')
+    cxxn.apply_load('LC 1', 1, 50, 'Permanent')
+
     cxxn.decompose_load()
 
     pass
